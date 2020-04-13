b2fbadb0e9c2d1f826771c0a7854d017e4276b60
Samarth Tambad <tambad.samarth@gmail.com>
2020-04-13 17:38:34 -0400

    added helper functions for launch script

--- network/network.sh
+++ network/network.sh
@@ -132,3 +132,124 @@ function checkPrereqs() {
   fi
 }
 
+# Generate the needed certificates, the genesis block and start the network.
+function networkUp () {
+  checkPrereqs
+  # If we are in dev mode, we move to the devmode directory
+  if [ "$DEV_MODE" = true ] ; then
+     pushd ./devmode
+     export FABRIC_CFG_PATH=${PWD}
+  fi
+  # generate artifacts if they don't exist
+  if [ ! -d "crypto-config" ]; then
+    generateCerts
+    replacePrivateKey
+    generateChannelArtifacts
+  fi
+  # Create folder for docker network logs
+  LOG_DIR=$(dirname $LOG_FILE)
+  if [ ! -d $LOG_DIR ]
+  then
+    mkdir -p $LOG_DIR
+  fi
+  IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE up >$LOG_FILE 2>&1 &
+
+  if [ "$DEV_MODE" = true ] ; then
+     popd
+     export FABRIC_CFG_PATH=${PWD}
+  fi
+
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to start network"
+    exit 1
+  fi
+}
+
+# Upgrade the network from one version to another
+# If the new image tag (now in the IMAGETAG variable) is not passed in the command line using the "-i" switch:
+# 	this assumes that the new iamge has already been tagged with "latest".
+# Stop the orderer and peers, backup the ledger from orderer and peers, cleanup chaincode containers and images
+# and relaunch the orderer and peers with latest tag
+function upgradeNetwork () {
+  docker inspect  -f '{{.Config.Volumes}}' orderer.justice.com |grep -q '/var/hyperledger/production/orderer'
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! This network does not appear to be using volumes for its ledgers, did you start from fabric-samples >= v1.0.6?"
+    exit 1
+  fi
+
+  LEDGERS_BACKUP=./ledgers-backup
+
+  # create ledger-backup directory
+  mkdir -p $LEDGERS_BACKUP
+
+  export IMAGE_TAG=$IMAGETAG
+  COMPOSE_FILES="-f $COMPOSE_FILE"
+
+  echo "Upgrading orderer"
+  docker-compose $COMPOSE_FILES stop orderer.justice.com
+  docker cp -a orderer.justice.com:/var/hyperledger/production/orderer $LEDGERS_BACKUP/orderer.justice.com
+  docker-compose $COMPOSE_FILES up --no-deps orderer.justice.com
+
+  for PEER in peer0.fbi.justice.com peer0.nypd.justice.com peer0.njsp.justice.com peer0.judiciary.justice.com; do
+    echo "Upgrading peer $PEER"
+
+    # Stop the peer and backup its ledger
+    docker-compose $COMPOSE_FILES stop $PEER
+    docker cp -a $PEER:/var/hyperledger/production $LEDGERS_BACKUP/$PEER/
+
+    # Remove any old containers and images for this peer
+    CC_CONTAINERS=$(docker ps | grep dev-$PEER | awk '{print $1}')
+    if [ -n "$CC_CONTAINERS" ] ; then
+        docker rm -f $CC_CONTAINERS
+    fi
+    CC_IMAGES=$(docker images | grep dev-$PEER | awk '{print $1}')
+    if [ -n "$CC_IMAGES" ] ; then
+        docker rmi -f $CC_IMAGES
+    fi
+
+    # Start the peer again
+    docker-compose $COMPOSE_FILES up --no-deps $PEER
+  done
+}
+
+# Bring down running network
+function networkDown () {
+  # If we are in dev mode, we move to the devmode directory
+  if [ "$DEV_MODE" = true ] ; then
+     pushd ./devmode
+  fi
+
+  docker-compose -f $COMPOSE_FILE down --volumes
+
+  for PEER in peer0.fbi.justice.com peer0.nypd.justice.com peer0.njsp.justice.com peer0.judiciary.justice.com; do
+    # Remove any old containers and images for this peer
+    CC_CONTAINERS=$(docker ps -a | grep dev-$PEER | awk '{print $1}')
+    if [ -n "$CC_CONTAINERS" ] ; then
+      docker rm -f $CC_CONTAINERS
+    fi
+  done
+
+  if [ "$DEV_MODE" = true ] ; then
+     popd
+  fi
+}
+
+# Delete network artifacts
+function networkClean () {
+  #Cleanup the chaincode containers
+  clearContainers
+  #Cleanup images
+  removeUnwantedImages
+  # If we are in dev mode, we move to the devmode directory
+  if [ "$DEV_MODE" = true ] ; then
+     pushd ./devmode
+  fi
+  # remove orderer block and other channel configuration transactions and certs
+  rm -rf channel-artifacts crypto-config
+  # remove client certs 
+  rm -rf client-certs
+  if [ "$DEV_MODE" = true ] ; then
+     popd
+  fi
+}
+
