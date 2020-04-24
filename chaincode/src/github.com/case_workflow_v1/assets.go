1b5108dbae60f9600f28ac117bfc14b81ecdee72
Samarth Tambad <tambad.samarth@gmail.com>
2020-04-24 12:08:11 -0400

    added asset types
    Case asset defines world state. Contains Suspect and EvidenceItem types within.

--- chaincode/src/github.com/case_workflow_v1/assets.go
+++ chaincode/src/github.com/case_workflow_v1/assets.go
@@ -1,1 +1,25 @@
 package main
+
+import "time"
+
+type EvidenceItem struct {
+	Type 		EvidenceType	`json:"type"`			// type of evidence
+	CreatedAt	time.Time		`json:"created"`		// time of logging evidence
+	Desc 		string			`json:"description"`	// description of the evidence
+	// yet to add EvidenceType specific elements
+}
+
+type Suspect struct {
+	Id			int				`json:"id"`			// unique identifier
+	Name		string			`json:"name"`		// suspect's real name
+	Status		SuspectStatus	`json:"status"`		// status of the suspect
+	Evidence	[]EvidenceItem	`json:"evidence"`	// evidence against a particular suspect
+}
+
+type Case struct {
+	Id			int				`json:"id"`			// unique identifier
+	FileDate	time.Time		`json:"file"`		// date of case filing
+	Suspects	[]Suspect		`json:"suspects"`	// suspects involved in the case
+	Evidence	[]EvidenceItem	`json:"evidence"`	// evidence collected not related to a particular suspect
+	Status		CaseStatus		`json:"status"`		// current status of the case
+}
