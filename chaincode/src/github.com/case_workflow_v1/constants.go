5715aee80a2991f138345c64bec8e8c18d2e4013
Samarth Tambad <tambad.samarth@gmail.com>
2020-04-24 11:59:07 -0400

    add type constants
    add type enumeration for the following:
    1. EvidenceType
    	PhysicalEvidence = 0
    	ForensicEvidence = 1
    	DigitalEvidence = 2
    	DocumentaryEvidence = 3
    	DemonstrativeEvidence = 4
    	TestimonialEvidence = 5
    
    2. SuspectStatus
    	UnderInvestigation = 0
    	Eliminated = 1
    
    3. CaseStatus
    	InTrial = 0
    	Closed = 1

---
+++ chaincode/src/github.com/case_workflow_v1/constants.go
@@ -0,0 +1,23 @@
+package main
+
+type EvidenceType int
+const (
+	PhysicalEvidence EvidenceType = iota	// in the form of a physical object
+	ForensicEvidence						// scientific in nature
+	DigitalEvidence							// emails, hard drives, cell phone logs, etc
+	DocumentaryEvidence						// contracts, wills, invoices, etc.
+	DemonstrativeEvidence					// photographs, videos, sound recordings, x-rays, etc.
+	TestimonialEvidence						// by a witness
+)
+
+type SuspectStatus int
+const (
+	UnderInvestigation SuspectStatus = iota
+	Eliminated
+)
+
+type CaseStatus int
+const(
+	InTrial CaseStatus = iota
+	Closed
+)
