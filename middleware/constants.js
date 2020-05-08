9e4d7bb81ff559b8c8f8fefa6b3bb6c07f9dce87
samarthtambad <tambad.samarth@gmail.com>
2020-05-08 12:26:16 -0400

    export constants from module

--- middleware/constants.js
+++ middleware/constants.js
@@ -46,3 +46,47 @@ var ACCEPT_ALL = {
         '0-of': []
     }
 };
+
+// define other constants
+
+// paths
+var chaincodeLocation = '../chaincode';
+var networkId = 'justice-network';
+var networkConfig = './config.json';
+var networkLocation = '../network';
+var channelConfig = 'channel-artifacts/channel.tx'; //
+
+// organizations
+var NYPD_ORG = 'nypd';
+var FBI_ORG = 'fbi';
+var NJSP_ORG = 'njsp';
+var JUDICIARY_ORG = 'judiciary';
+
+// channel
+var CHANNEL_NAME = 'justicechannel';
+var CHAINCODE_PATH = 'github.com/case_workflow_v1';
+var CHAINCODE_ID = 'justicecc';
+var CHAINCODE_VERSION = 'v0';
+
+// endorsement policy
+var TRANSACTION_ENDORSEMENT_POLICY = THREE_OF_FOUR_ORG_MEMBERS;
+
+// export constants from module
+module.exports = {
+    tempdir: tempdir,   // for client-certs
+    chaincodeLocation: chaincodeLocation,
+    networkId: networkId,
+    networkConfig: networkConfig,
+    networkLocation: networkLocation,
+    channelConfig: channelConfig,
+    NYPD_ORG: NYPD_ORG,
+    FBI_ORG: FBI_ORG,
+    NJSP_ORG: NJSP_ORG,
+    JUDICIARY_ORG: JUDICIARY_ORG,
+    CHANNEL_NAME: CHANNEL_NAME,
+    CHAINCODE_PATH: CHAINCODE_PATH,
+    CHAINCODE_ID: CHAINCODE_ID,
+    CHAINCODE_VERSION: CHAINCODE_VERSION,
+    ACCEPT_ALL: ACCEPT_ALL,
+    TRANSACTION_ENDORSEMENT_POLICY: TRANSACTION_ENDORSEMENT_POLICY
+};
