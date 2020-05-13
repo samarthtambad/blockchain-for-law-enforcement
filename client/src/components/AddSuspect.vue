66c97ec8f9794201245695e963cc5a312f3cd0bf
samarthtambad <tambad.samarth@gmail.com>
2020-05-12 21:13:36 -0400

    new components working:

--- client/src/components/AddSuspect.vue
+++ client/src/components/AddSuspect.vue
@@ -1,17 +1,42 @@
 <template>
     <div>
         <h2> Add Suspect </h2>
+        <form>
+            <div class="form-group">
+                <label for="id">ID</label>
+                <input type="text" v-model="id" name="id" class="form-control" />
+            </div> 
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" v-model="name" name="name" class="form-control" />
+            </div>
+            <div class="form-group">
+                <button type="submit" @click="addSuspect" class="btn btn-primary">Add Suspect</button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
+    props: {
+        caseId: { 
+            required: true,
+        },
+    },
+    data() {
+        return {
+            id: "",
+            name: ""
+        }
+    },
     methods: {
         addSuspect(e) {
             e.preventDefault()
-            axios.post('http://localhost:4000/chaincode/addSuspectToCase', 
+            axios.post('http://localhost:4000/chaincode/addSuspectToCase',
                 {
-                    args: [this.caseId, "suspect#1", "James Bond"],
+                    args: [this.caseId, this.id, this.name],
                     ccversion: "v1"
                 },
                 { headers: {
@@ -22,25 +47,45 @@ export default {
             .then(response => {
                 console.log(response)
 
-                if(response.data.success) {
-                    console.log("Fetched case successfully");
-                    var data = JSON.parse(response.data.message)
-                    console.log(data)
-                    this.caseData = data
-                } else {
-                    console.log(response)
+                if(!response.data.success) {
+                    console.log("Could not add suspect")
+                    return
                 }
 
+                console.log("Added suspect successfully");
+                this.$router.back()
             })
             .catch(function (error) {
                 console.error(error.response);
             });
-
         }
+    },
+    computed: {
+        auth() {
+            return this.$store.getters.userAuth
+        },
     }
 }
 </script>
 
 <style scoped>
-
+button {
+    padding: 5px;
+    border-radius: 5px;
+    margin: 10px;
+}
+h1, h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+}
 </style>
