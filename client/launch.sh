b6b3a76f03936ebbd115814d81c331ef9433ee9b
samarthtambad <tambad.samarth@gmail.com>
2020-05-12 20:22:44 -0400

    modified launch script to change dir to scripts

--- client/launch.sh
+++ client/launch.sh
@@ -4,7 +4,8 @@
 # setup the network, create/join channel, install/instantiate chaincode, etc
 # by calling server APIs
 
-echo -e "\nYour are at path: " $(pwd)
+cd $GOPATH/src/blockchain-for-law-enforcement/application/client_scripts
+echo -e "\nHave now switched to " $(pwd)
 
 ./scripts/adminLogin.sh
 ./scripts/createChannel.sh
@@ -14,6 +15,8 @@ echo -e "\nYour are at path: " $(pwd)
 ./scripts/userLogin.sh
 ./scripts/invoke_registerCase.sh
 
+cd $GOPATH/src/blockchain-for-law-enforcement/client
+echo -e "\nHave now switched to " $(pwd)
 
 # launch vuejs server for serving the user interface
 ./node_modules/.bin/vue-cli-service serve
